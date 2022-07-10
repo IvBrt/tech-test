b80344a062cf235e6f86d14cf6161e107ed02dbc
IvBrt <crstn.b7@gmail.com>
2022-07-09 19:07:30 -0500

    instalación de scroll infinito y cambio de imagen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
