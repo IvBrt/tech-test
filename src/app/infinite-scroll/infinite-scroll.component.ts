491d01d4591cc0d12d209cc3e38a522aa4d59915
IvBrt <crstn.b7@gmail.com>
2022-07-10 03:03:54 -0500

    se repite 7 veces el contenedor en consola

--- src/app/infinite-scroll/infinite-scroll.component.ts
+++ src/app/infinite-scroll/infinite-scroll.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-infinite-scroll',
   templateUrl: './infinite-scroll.component.html',
-  styleUrls: ['./infinite-scroll.component.css']
+  // styleUrls: ['./infinite-scroll.component.css']
 })
 export class InfiniteScrollComponent implements OnInit {
+  @ViewChild('container') container: ElementRef;
+  public list: any[];
 
   constructor() { }
 
   ngOnInit() {
   }
+  onScrollDown() {
+      let repeatConainer = this.container.nativeElement;
+      this.list = [];
+      for(let i = 0; i < 7; i++){
+        this.list.push(repeatConainer)
+      }
+      console.log(this.list)
+    }
 
 }
